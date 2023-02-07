a7a5b6052c75bca5f918cfd81a2638f521dfc64c
Teo <teo.orthlieb@gmail.com>
2023-02-07 09:55:49 +0100

    now using pathes + trying to make it work in other projects

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,23 @@ use std::collections::HashSet;
 use itertools::Itertools;
 use lazy_static::lazy_static;
 use std::fs;
-const IMG_PATH: &str = "assets/72x72/";
+use std::path::{Path, PathBuf};
 
 lazy_static! {
+    static ref ASSETS_PATH: PathBuf = Path::new(env!("CARGO_MANIFEST_DIR")).join("assets").join("72x72");
     static ref FILE_NAMES: HashSet<String> = HashSet::from_iter(
-        fs::read_dir(IMG_PATH).unwrap()
+        fs::read_dir(&*ASSETS_PATH).unwrap()
             .into_iter()
             .map(|path| path.unwrap().file_name().into_string().unwrap())
     );
 }
 
-pub fn get_twemoji(text: &str) -> Option<String> {
+pub fn get_twemoji(text: &str) -> Option<PathBuf> {
     let fname = text.chars().into_iter()
         .map(|c| format!("{:x?}", c as u32))
         .join("-")+".png";
     if FILE_NAMES.contains(&fname) {
-        Some(format!("{}{}", IMG_PATH, fname))
+        Some(ASSETS_PATH.join(&fname))
     } else {
         None
     }
@@ -25,10 +26,19 @@ pub fn get_twemoji(text: &str) -> Option<String> {
 
 #[cfg(test)]
 mod tests {
+    use std::{path::{PathBuf}, fs::File};
     use crate::get_twemoji;
+
+    fn print_call(text: &str) -> Option<PathBuf> {
+        let opt_path: Option<PathBuf> = get_twemoji("💻");
+        println!("{} -> {:?}", text, opt_path);
+        opt_path
+    }
 
     #[test]
     fn it_works() {
-        println!("{:?}", get_twemoji("💻"))
+        let opt_path: Option<PathBuf> = print_call("💻");
+        assert!(opt_path.is_some());
+        assert!(File::open(opt_path.unwrap()).is_ok());
     }
 }
